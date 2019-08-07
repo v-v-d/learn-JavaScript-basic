ec3ede9e0762546a61379ee09e5c4facf98a5412
v-v-d <v-v-d@bk.ru>
2019-08-08 01:50:41 +0300

    task 2 done

--- lesson_1/2.js
+++ lesson_1/2.js
@@ -1,2 +1,11 @@
+'use strict';
+
 // Объявить две переменные: admin и name. Записать в name строку "Василий". Скопировать значение из name в admin.
 // Вывести в консоль переменную admin (должно вывести "Василий").
+
+let admin = null;
+let name = 'Василий';
+
+admin = name;
+
+console.log(admin);
