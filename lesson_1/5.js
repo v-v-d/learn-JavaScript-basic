d41436eb8db3ed51e05077bcefca44fe7f5fb701
v-v-d <v-v-d@bk.ru>
2019-08-08 03:08:07 +0300

    task 5 done

--- lesson_1/5.js
+++ lesson_1/5.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Пользователь, с помощью команды prompt, вводит номер билета - 6 цифр. Программа определяет является ли счастливым
 // данный билетик и выводит соответстующее сообщение в консоль. Счастливый билет - билет, у которого сумма первых трех
 // цифр равна сумме последних трех цифр номера билета.
@@ -11,3 +13,26 @@
 //   console.log('Две переменные a и b равны.');
 // }
 // ```
+
+let ticketNumber = parseInt(prompt('Input ticket number:'));
+
+let ticketNumberSum1 = null;
+let ticketNumberSum2 = null;
+
+while (ticketNumber) {
+    if (ticketNumber.toString().length > 3) {
+        ticketNumberSum2 += ticketNumber % 10;
+        ticketNumber -= ticketNumber % 10;
+        ticketNumber /= 10;
+    } else {
+        ticketNumberSum1 += ticketNumber % 10;
+        ticketNumber -= ticketNumber % 10;
+        ticketNumber /= 10;
+    }
+}
+
+if (ticketNumberSum1 === ticketNumberSum2) {
+    console.log('Билет счастливый')
+} else {
+    console.log('Билет не счастливый')
+}
