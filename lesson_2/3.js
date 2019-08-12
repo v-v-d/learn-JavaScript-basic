7f16c95d848b92912b87de0640a4b8201b999621
v-v-d <v-v-d@bk.ru>
2019-08-13 02:36:59 +0300

    task 3 done

--- lesson_2/3.js
+++ lesson_2/3.js
@@ -5,3 +5,23 @@
 // числом);
 // - если а и b отрицательные, вывести их произведение;
 // - если а и b разных знаков, вывести их сумму;
+'use strict';
+
+/**
+ * Get random integer ranging from a min number to max number
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+const getRandInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+let a = getRandInt(-100, 100);
+let b = getRandInt(-100, 100);
+
+if (a >= 0 && b >= 0) {
+    console.log(a - b)
+} else if (a < 0 && b < 0) {
+    console.log(a * b)
+} else if ((a < 0 && b >= 0) || (a >= 0 && b < 0)) {
+    console.log(a + b)
+}
