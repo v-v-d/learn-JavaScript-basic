d27f683375aae586201dd045cd5d4a5eaf82dd5f
v-v-d <v-v-d@bk.ru>
2019-08-13 04:09:51 +0300

    task 3 mistakes fixed

--- lesson_2/3.js
+++ lesson_2/3.js
@@ -22,6 +22,6 @@ if (a >= 0 && b >= 0) {
     console.log(a - b)
 } else if (a < 0 && b < 0) {
     console.log(a * b)
-} else if ((a < 0 && b >= 0) || (a >= 0 && b < 0)) {
+} else {
     console.log(a + b)
 }
