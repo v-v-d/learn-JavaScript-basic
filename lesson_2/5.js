fec7ecadf0df490c191c92754107e7c349f10d90
v-v-d <v-v-d@bk.ru>
2019-08-13 03:16:09 +0300

    task 5 done

--- lesson_2/5.js
+++ lesson_2/5.js
@@ -4,3 +4,48 @@
 // переданного значения операции (использовать switch) выполнить одну из арифметических
 // операций
 // (использовать функции из задания 4) и вернуть полученное значение.
+
+/**
+ * Make one of the arithmetic operations with passed arguments depending on the passed operation value.
+ * @param {number} arg1 1st operand
+ * @param {number} arg2 2nd operand
+ * @param {string} operation The name of the operation
+ * @returns {number}
+ */
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+        case '+':
+        case 'сложение':
+        case 'Сложение':
+        case 'addition':
+        case 'Addition':
+            return myAddition(arg1, arg2);
+        case '-':
+        case 'вычитание':
+        case 'Вычитание':
+        case 'subtraction':
+        case 'Subtraction':
+            return mySubtraction(arg1, arg2);
+        case '/':
+        case ':':
+        case 'деление':
+        case 'Деление':
+        case 'division':
+        case 'Division':
+            return myDivision(arg1, arg2);
+        case '*':
+        case 'умножение':
+        case 'Умножение':
+        case 'multiplication':
+        case 'Multiplication':
+            return myMultiplication(arg1, arg2);
+    }
+}
+
+let numb1 = getRandInt(-100, 100);
+let numb2 = getRandInt(-100, 100);
+
+console.log(mathOperation(numb1, numb2, '+'));
+console.log(mathOperation(numb1, numb2, 'вычитание'));
+console.log(mathOperation(numb1, numb2, ':'));
+console.log(mathOperation(numb1, numb2, 'Multiplication'));
