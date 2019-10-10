73ce88717cad851320682ad3162c053e299280bc
v-v-d <v-v-d@bk.ru>
2019-10-11 02:46:34 +0300

    task 5 done

--- lesson_3/5.js
+++ lesson_3/5.js
@@ -11,3 +11,38 @@
 // индекс этого массива.
 // 2 Получить и вывести в console.log минимальное значение найденное в массиве,
 // который мы получили в первом пункте.
+'use strict';
+
+const arr = [
+  [2, 4, 6],
+  [1, 5, 10],
+  [7, 4, 1],
+];
+
+console.log(arr);
+
+let maxSum = 0;
+let maxAmountArr;
+
+for (let i = 0; i < arr.length; i++) {
+  let arrSum = 0;
+  for (let j = 0; j < arr[i].length; j++) {
+    arrSum += arr[i][j]
+  }
+  if (arrSum > maxSum) {
+    maxSum = arrSum;
+    maxAmountArr = i;
+  }
+}
+
+console.log(`Максимальная сумма чисел равная ${maxSum} у массива ${arr[maxAmountArr]}`);
+
+let minNum = arr[maxAmountArr][0];
+
+for (let i = 0; i < arr[maxAmountArr].length; i++) {
+  if (arr[maxAmountArr][i] < minNum) {
+    minNum = arr[maxAmountArr][i]
+  }
+}
+
+console.log(`Минимальное значение в массиве ${arr[maxAmountArr]} это ${minNum}`);
