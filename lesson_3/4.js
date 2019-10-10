07614985d254e563bc9b4d642382ed532eebffeb
v-v-d <v-v-d@bk.ru>
2019-10-11 02:17:53 +0300

    task 4 done

--- lesson_3/4.js
+++ lesson_3/4.js
@@ -1,2 +1,34 @@
 // 4*. С помощью цикла while вывести все простые числа в промежутке от 0 до 100
 // (можно без оптимизаций).
+'use strict';
+
+function isPrimeNumber(num) {
+    for (let i = 2; i <= Math.round(num ** 0.5 + 1.2); i++) {
+        if (num % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+function getPrimeNumber(toNumber) {
+    if (toNumber <= 1) {
+        return
+    }
+
+    if (toNumber === 2) {
+        console.log(2);
+        return;
+    }
+
+    console.log(2);
+    console.log(3);
+
+    for (let i = 5; i <= toNumber; i++) {
+        if (isPrimeNumber(i)) {
+            console.log(i)
+        }
+    }
+}
+
+getPrimeNumber(100);
