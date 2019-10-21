4ba2c626ff44dfdab7eb9a58c6158d6c1a5fcec7
v-v-d <v-v-d@bk.ru>
2019-10-22 00:29:16 +0300

    task 1 done

--- lesson_4/1.js
+++ lesson_4/1.js
@@ -15,3 +15,34 @@
 //
 // Если число было передано вне [0, 999] диапазона, не целое число или вообще не число,
 // необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
+'use strict';
+
+/**
+ * Сonverts input number to object where property is a digit number
+ * @param {string} num Number from 0 to set into function value
+ * @return {object} Object from number
+ */
+function getObjFromNum(num) {
+  const objFromNum = {};
+
+  let namesOfDigits = ['first', 'second', 'third'];
+  let maxNumLen = namesOfDigits.length;
+
+  if (parseInt(num) == num && num.length <= maxNumLen) {
+    for (let i = 1; i < maxNumLen + 1; i++) {
+      if (num[num.length - i]) {
+        objFromNum[namesOfDigits[i - 1] + 'Digit'] = parseInt(num[num.length - i])
+      } else {
+        objFromNum[namesOfDigits[i - 1] + 'Digit'] = 0
+      }
+    }
+  } else {
+    console.log(`Переданное число вне диапазона [0, ${10**maxNumLen - 1}], не целое число или вообще не число`)
+  }
+
+  return objFromNum
+}
+
+let userNum = prompt('Введите число в диапазоне [0, 999]: ');
+
+getObjFromNum(userNum);
